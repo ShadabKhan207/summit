63b31d16a9e59310b9f0348a352c022bd2247305
unknown <shadab@8848digital.com>
2024-02-06 17:00:29 +0530

    feat:mobile otp login :sk

---
+++ pages/login-through-mobile-otp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginThroughMobileOtp from '../components/Auth/LoginThroughMobileOtp'
+
+const index = () => {
+  return (
+   <>
+   <LoginThroughMobileOtp/>
+   </>
+  )
+}
+
+export default index
